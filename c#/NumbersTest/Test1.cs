8b8791e8f1c4539caf7a7d9d2dd90903749484c4
filipRadke <filip.radke@onet.pl>
2025-11-20 11:29:27 +0100

    C# testing

---
+++ c#/NumbersTest/Test1.cs
@@ -0,0 +1,15 @@
+﻿using nu
+namespace NumbersTest
+{
+    [TestClass]
+    public sealed class OperationsTest
+    {
+        usin
+
+        [TestMethod]
+        public void AddTest()
+        {
+            Assert.AreEqual(2,Add)
+        }
+    }
+}
