65a765eaab7910b44c85e17e236b9eb8f54b0349
filipRadke <filip.radke@onet.pl>
2025-11-27 13:03:41 +0100

    New test; Screenshots of code and tests

--- cpp/cppUnitTests/TestApp/TestApp.cpp
+++ cpp/cppUnitTests/TestApp/TestApp.cpp
@@ -7,8 +7,27 @@ bool isDivisible(int x, int y) {
     return false;
 }
 
+bool isPrime(int x) {
+    if(x <= 1) return false;
+    for (int i = 2; i * i <= x; i++) {
+        if(x % i == 0) return false;
+    }
+    return true;
+}
+
+int summed(int x, int y) {
+    int len = 10;
+    while (1) {
+        if(len <= y) len*=10;
+        else break;
+    }
+    x *= len;
+
+    return x + y;
+}
+
 int main()
 {
-    cout << "Hello World!\n";
+    cout << isDivisible(10,2) << endl << isPrime(10) << endl << summed(10,15);
 }
 
--- cpp/cppUnitTests/UnitTest/UnitTest.cpp
+++ cpp/cppUnitTests/UnitTest/UnitTest.cpp
@@ -6,17 +6,45 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest
 {
-	TEST_CLASS(UnitTest)
+	TEST_CLASS(TestIsDivisible)
 	{
 	public:
 		
-		TEST_METHOD(UnitIsDivisibleT)
+		TEST_METHOD(IsDivisibleT)
 		{
 			Assert::AreEqual(true, isDivisible(4, 2));
 		}
-		TEST_METHOD(UnitIsDivisibleF)
+		TEST_METHOD(IsDivisibleF)
 		{
 			Assert::AreEqual(true, isDivisible(5, 3));
+		}
+	};
+
+	TEST_CLASS(TestIsPrime)
+	{
+	public:
+
+		TEST_METHOD(IsPrimeT)
+		{
+			Assert::AreEqual(true, isPrime(7));
+		}
+		TEST_METHOD(IsPrimeF)
+		{
+			Assert::AreEqual(true, isPrime(10));
+		}
+	};
+
+	TEST_CLASS(TestSummed)
+	{
+	public:
+
+		TEST_METHOD(SummedT1)
+		{
+			Assert::AreEqual(1015, summed(10,15));
+		}
+		TEST_METHOD(SummedT2)
+		{
+			Assert::AreEqual(210, summed(2,10));
 		}
 	};
 }
